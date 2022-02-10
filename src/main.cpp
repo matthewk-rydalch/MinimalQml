53fdb33aa2f700b4994e483f061d489dd3114af8
Matthew Rydalch <matthew@utopiacompression.com>
2022-02-10 14:57:06 -0700

    Removed application window implementation, and made the source path local.

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,8 @@ int main(int argc, char** argv)
     QApplication app(argc, argv);
     QQuickView view;
 
-    view.setSource(QUrl::fromLocalFile("/home/matt/Projects/sandbox/qml_sandbox/src/main.qml"));
+    view.setSource(QUrl(QStringLiteral("qrc:/main.qml")));
+    view.show();
 
     return app.exec();
 }
--- src/main.qml
+++ src/main.qml
@@ -1,11 +1,10 @@
 import QtQuick 2.0
 import QtQuick.Controls 1.0
 
-ApplicationWindow
+Rectangle
 {
     visible: true
     width: 640
     height: 480
-    title: qsTr("Minimal Qml")
 }
 
